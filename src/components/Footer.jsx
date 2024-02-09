648fc56c9e7f5d6c3c1e1bb879a2ea7b04fcdc28
VladoLov <vladislav.lovric@gmail.com>
2024-02-09 18:33:38 +0100

    trid update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,24 @@ const Footer = () => {
       <PageLinks parentClass="footer-links" itemClass="footer-link" />
       <div className="contact-center">
         <div className="gird-left">
-          <h4>Contact Us</h4>
+          <h4 className="footer-information">Contact Us:</h4>
+          <p>
+            {/* Click{" "} */}
+            <a
+              href="https://www.google.com/maps?q=43.350764, 17.807767"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Dr.Mile Budaka 97 Mostar, Bosnia and Herzegovina
+            </a>
+            {/*{" "} */}
+          </p>
+          <p>
+            <a href="tel:+38761390879">+38761390879</a>{" "}
+          </p>
+          <p>
+            <a href="mailto:lovric.v@hotmail.com">irenalovric@example.com</a>
+          </p>
 
           <ul className="footer-icons">
             {socialLinks.map((link) => {
@@ -27,9 +44,9 @@ const Footer = () => {
             <iframe
               title="Google Map Location"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2901.2579161028993!2d17.805211176840594!3d43.350715371941156!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x134b43a1d26e7d99%3A0x2649f53d9391c70f!2sDr.%20Mile%20Budaka%2097%2C%20Mostar%2088000!5e0!3m2!1shr!2sba!4v1707315201331!5m2!1shr!2sba"
-              width="600"
-              height="450"
-              style={{ border: 0 }}
+              // width="600"
+              // height="450"
+              // style={{ border: 0 }}
               allowFullScreen=""
               loading="lazy"
             ></iframe>
@@ -39,6 +56,23 @@ const Footer = () => {
         {/* Additional Footer Icons */}
         <div className="gird-right">
           <h4>Visit us in Cavtat</h4>
+          <p>
+            <a
+              href="https://www.google.com/maps?q=42.57747793806969, 18.222046287495345"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Stjepana Radića 4C,20210 Cavtat, Croatia
+            </a>
+          </p>
+          <p>
+            <a href="tel:+385993771871">+385993771871</a>{" "}
+          </p>
+          <p>
+            <a href="mailto:lovric_boris@hotmail.com">
+              lovric_boris@hotmail.com
+            </a>
+          </p>
           <ul className="footer-icons">
             {otherLinks.map((link) => {
               return (
@@ -50,7 +84,7 @@ const Footer = () => {
       </div>
 
       <p className="copyright">
-        copyright &copy; Backroads travel tours company
+        copyright &copy;
         <span id="date">{new Date().getFullYear()}</span>. all rights reserved
       </p>
     </footer>
